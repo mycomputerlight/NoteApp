473ac5e1774bb063b951c107c1a87b7cf4d3347c
serafettinkcktas <kucuktas255@gmail.com>
2026-07-28 12:02:16 +0300

    update: jwt credentials added new db connection method added

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NoteApp.Auth;
 using NoteApp.Data;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,24 +16,43 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddOpenApi();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt_Token:Issuer"],
+            ValidAudience = builder.Configuration["Jwt_Token:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt_Token:SecretKey"] ?? string.Empty)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+builder.Services.AddSingleton<JwtTokenHelper>();
 //VERİTABANI BAĞLANTISI
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DockerDbConnection"))); // kendi veritabanim icin ekledim 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwagger();
+    app.MapScalarApiReference();
     app.UseSwaggerUI();
   
 }
 
 app.UseHttpsRedirection();
 
-//app.UseAuthentication(); //?
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllers();
